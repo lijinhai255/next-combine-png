5e5c64bb74e06b150f6fad64cd36d34450cd1658
lijinhai255 <18854109500@163.com>
2024-10-26 19:09:52 +0800

    Update deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: "export", // 改为静态导出
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
